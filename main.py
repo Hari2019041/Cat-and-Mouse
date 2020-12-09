dfa5427da9049ad6fb83b5e03d0d4571179b51d4
Hari2019041 <hari19041@iiitd.ac.in>
2020-12-09 19:29:24 +0530

    Basic Template code

---
+++ main.py
@@ -0,0 +1,39 @@
+import pygame
+
+pygame.init()
+
+
+class Mouse:
+    def __init__(self):
+        pass
+
+    def show(self):
+        pass
+
+
+class Cat:
+    def __init__(self):
+        pass
+
+    def show(self):
+        pass
+
+
+class Pond:
+    def __init__(self):
+        pass
+
+    def show(self):
+        pass
+
+
+class Simulation:
+    def __init__(self):
+        pass
+
+    def startSimulation(self):
+        pass
+
+
+def main():
+    pass
