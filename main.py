41c60faba4d712bc3c577e57f18bdbdff1e5dc9b
Hari2019041 <hari19041@iiitd.ac.in>
2021-09-24 21:41:57 +0530

    Program Configurations

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 import pygame
 
 pygame.init()
+pygame.font.init()
+pygame.mixer.init()
 
+WIDTH = 600
+HEIGHT = 600
+TITLE = "Cat and Mouse"
+ICON = pygame.image.load('icon.png')
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+def setWindow(TITLE, ICON):
+    pygame.display.set_caption(TITLE)
+    pygame.display.set_icon(ICON)
+
+#Colors
+WHITE = (0, 0, 0)
+BLACK = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
 
 class Mouse:
     def __init__(self):
@@ -37,3 +55,6 @@ class Simulation:
 
 def main():
     pass
+
+if __name__ == '__main__':
+    main()
