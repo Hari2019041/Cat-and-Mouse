a06d9cc87e104ba73491bd274b850432ffd164b6
Hari2019041 <hari19041@iiitd.ac.in>
2021-09-25 23:03:34 +0530

    Init of Pond and added icon.png

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ pygame.mixer.init()
 
 WIDTH = 600
 HEIGHT = 600
+MARGIN = 50
 TITLE = "Cat and Mouse"
 ICON = pygame.image.load('icon.png')
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -38,11 +39,15 @@ class Cat:
 
 
 class Pond:
-    def __init__(self):
-        pass
+    def __init__(self, color=BLUE):
+        self.x = WIDTH//2
+        self.y = HEIGHT//2
+        self.center = (self.x, self.y)
+        self.color = color
+        self.size = WIDTH//2 - MARGIN
 
     def show(self):
-        pass
+        pygame.draw.circle(SCREEN, self.color, self.center, self.size, self.size)
 
 
 class Simulation:
